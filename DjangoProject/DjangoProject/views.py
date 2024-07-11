d3719abdd94209e6d3d1192a5d2a646434947e41
Dhruv Maisuria <dhruvmaisuriya123@gmail.com>
2024-07-11 10:38:27 +0530

    First commit

---
+++ DjangoProject/DjangoProject/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+def home(request):
+    # return HttpResponse("Hello, This is my first Django Project, you are at Home page. ")
+    return render(request, 'website/index.html')
+
+def about(request):
+    # return HttpResponse("Hello, This is my first Django Project, you are at About page. ")
+    return render(request,'website/about.html')
+
+def contact(request):
+    # return HttpResponse("Hello, This is my first Django Project, you are at Contact page. ")
+    return render(request,'website/contact.html')
